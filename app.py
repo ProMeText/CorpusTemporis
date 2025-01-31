3cd833a2abf64096c9b5176c65e20076299de83b
carolisteia <carolina.macedo@hotmail.fr>
2025-01-31 09:21:56 +0100

    fix derived

--- app.py
+++ app.py
@@ -204,12 +204,13 @@ with st.form(key="corpus_form", clear_on_submit=True):
             if xml_uploader :
                 bytes_data = xml_uploader.read()
                 filename = xml_uploader.name
+                txt_filename = xml_uploader.stem + '.txt'
                 Path(f'xmls/{filename}').write_bytes(bytes_data)
                 try:
                     formatted_xml = prittify_xml(bytes_data.decode('utf-8'))
                     Path(f'data/formatted_xmls/{filename}').write_text(formatted_xml)
                     derivated_txt = derive_txt(bytes_data.decode('utf-8'))
-                    Path(f'data/derived_txts/{filename.stem}.txt').write_text(derivated_txt)
+                    Path(f'data/derived_txts/{txt_filename}').write_text(derivated_txt)
                     st.success('Xml Formatted')
                 except Exception as ex:
                     st.error('Problem with xmls formatting and derive text')
